0346a7322ca560e3d6a93a6a7c2e0498278f1c27
CCulver831 <c.culver831@gmail.com>
2020-08-08 20:57:58 -0700

    Imported Tron demo

---
+++ PartyGameUnity/Assets/PlayerControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Chloe Culver
+//This script 
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField]
+    [Range(1, 20)]
+    private float speed = 1;
+
+    private Vector3 LookAt;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+    }
+    public void ActionButtons(string PartyName)
+    {
+
+    }
+    public void Move()
+    {
+        float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector3 newPosition = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        transform.LookAt(newPosition + transform.position);
+        transform.Translate(newPosition * speed * Time.deltaTime, Space.World);
+
+
+    }
+
+}
---
+++ PartyGameUnity/Assets/Scripts/SceneManager1.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager1 : MonoBehaviour
+{
+
+    public void OnButtonClicked()
+    {
+        SceneManager.LoadScene("Scene1");
+    }
+}
