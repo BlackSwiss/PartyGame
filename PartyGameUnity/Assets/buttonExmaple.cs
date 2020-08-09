ae2d64e00dea0537a86f436bbbf673b0d37730dc
BlackSwiss <dylan.ayala562@gmail.com>
2020-08-08 19:06:25 -0700

    1st push, random input button set up

---
+++ PartyGameUnity/Assets/buttonExmaple.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class buttonExmaple : MonoBehaviour {
+
+    string[] joystickArray = new string[]{"Joystick Button 0", "Joystick Button 1", "Joystick Button 2",
+    "Joystick Button 3","Joystick Button 4","Joystick Button 5"};
+    string[] ex = new string[] { "1", "2", "3", "4" };
+
+    System.Random r = new System.Random();
+
+    
+
+    public KeyCode key;
+
+    private Button button;
+
+    int count = 0;
+
+    private void Awake()
+    {
+        //ex = ex.OrderBy(x => r.Next()).ToArray();
+       // Debug.Log(ex[0]);
+        button = GetComponent<Button>();
+        /*joystickArray.Shuffle();
+        Debug.Log(joystickArray[0]);
+        */
+
+        joystickArray = joystickArray.OrderBy(x => r.Next()).ToArray();
+        Debug.Log(joystickArray[0]);
+    }
+
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (Input.GetButtonDown(joystickArray[0]))
+        {
+            count++;
+            button.onClick.Invoke();
+             Debug.Log("Pressed " + count);
+            //Debug.Log(joystickArray[0]);
+            joystickArray = joystickArray.OrderBy(x => r.Next()).ToArray();
+            Debug.Log(joystickArray[0]);
+
+        }
+	}
+
+}
