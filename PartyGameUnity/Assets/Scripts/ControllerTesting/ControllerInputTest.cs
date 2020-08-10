18ec18dfdc48c981ac2ac4c2ac6b8ae277313260
CCulver831 <c.culver831@gmail.com>
2020-08-09 22:15:07 -0700

    Debugging Local Multipayer

---
+++ PartyGameUnity/Assets/Scripts/ControllerTesting/ControllerInputTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ControllerInputTest : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    private ControllerInputTest Mover;
+    //For Joystick movement
+    Vector2 i_movement;
+    float moveSpeed = 5f; //MoveSpeed
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        var movers = FindObjectOfType<ControllerInputTest>();
+        var index = playerInput.playerIndex;
+        Mover = GetComponent<ControllerInputTest>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()  {
+        Move();
+    }
+    void Move() {
+        Vector3 movement = new Vector3(i_movement.x, 0, i_movement.y) * moveSpeed * Time.deltaTime;
+        transform.LookAt(movement + transform.position);
+        transform.Translate(movement, Space.World);
+    }
+    void OnMove(InputValue value) {
+        i_movement = value.Get<Vector2>(); //Moves the character on left joystick
+    }
+    public void SetInputVector(Vector2 direction){
+
+
+    }
+}
---
+++ PartyGameUnity/Assets/Scripts/ControllerTesting/PlayerInputHandler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
+//This script gets player Input and sends it over to the controller input script
+public class PlayerInputHandler : MonoBehaviour
+{
+    private Mover mover;
+    private PlayerInput playerInput;
+    [SerializeField]
+    private int playerIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        var index = playerInput.playerIndex;
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        var x = Input.GetAxisRaw("Horizontal");
+        var y = Input.GetAxisRaw("Vertical");
+        mover.SetInputVector(new Vector2(x, y));
+    }
+    public int GetPlayerIndex()
+    {
+        return playerIndex;
+    }
+    //Function from contrller
+    public void OnMove(CallbackContext context)
+    {
+       mover.SetInputVector(context.ReadValue<Vector2>()); //reads controller 
+    }
+}
+
