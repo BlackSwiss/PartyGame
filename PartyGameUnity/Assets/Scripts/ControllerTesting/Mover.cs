06b3a9db3289efad46406af63d66db35d9306e46
CCulver831 <c.culver831@gmail.com>
2020-08-11 13:07:44 -0700

    Messing with some contrller testing

--- PartyGameUnity/Assets/Scripts/ControllerTesting/Mover.cs
+++ PartyGameUnity/Assets/Scripts/ControllerTesting/Mover.cs
@@ -2,27 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Chloe Culver
+/*
+ * This script is in charge of Moving the player in the world space
+ * It's the literal game logic of moving an object
+ * 
+ * Attach this script to anything we want to move (I.e. the player)
+ * */
 public class Mover : MonoBehaviour
 {
     [SerializeField]
-    private int playerIndex = 0;
+    private int playerIndex = 0; //The controller num that is connected to this object
     [SerializeField]
-    private float moveSpeed = 5.81f;
+    private float moveSpeed = 5.81f; //How fast the object is moving
     //Move direction
-    private Vector3 moveDirection = Vector3.zero;
-    private Vector2 inputVector = Vector2.zero;
+    private Vector3 moveDirection = Vector3.zero; //Where we move in the world
+    private Vector2 inputVector = Vector2.zero; // Stick input of the X and Y values on a controller
 
     private CharacterController controller;
     private void Awake()
     {
-        controller = GetComponent<CharacterController>();
+        controller = GetComponent<CharacterController>(); //Grab character controller
     }
     public void SetInputVector(Vector2 direction)
     {
-        inputVector = direction;
+        inputVector = direction; //Setting the stick controls to the direction we want the player to move in
     }
 
-    public int GetPlayerIndex()
+    public int GetPlayerIndex() //return index of contrller (Player 1, Player 2)
     {
         return playerIndex;
     }
@@ -30,10 +37,11 @@ public class Mover : MonoBehaviour
     void Update()
     {
         {
+            //Get info from the stick as to how to move and translate that into 3D space
             Vector3 movement = new Vector3(inputVector.x, 0, inputVector.y) * moveSpeed * Time.deltaTime;
-            transform.LookAt(movement + transform.position);
+            transform.LookAt(movement + transform.position); //rotates player object in the direction it's moving to
             transform.Translate(movement, Space.World);
-            controller.Move(movement);
+            controller.Move(movement); //Moves the character from character contrller (may not be necessary)
         }
     }
 }
--- PartyGameUnity/Assets/Scripts/ControllerTesting/PlayerInputHandler.cs
+++ PartyGameUnity/Assets/Scripts/ControllerTesting/PlayerInputHandler.cs
@@ -8,20 +8,20 @@ using static UnityEngine.InputSystem.InputAction;
 public class PlayerInputHandler : MonoBehaviour
 {
     //ref to playerInput
-    private PlayerInput playerInput;
+    private PlayerInput playerInput; //The literal player Input of the controller
     private Mover mover; //ref to movement script
     void Awake()
     {
         playerInput = GetComponent<PlayerInput>();
         var movers = FindObjectsOfType<Mover>(); //Get player objects
-        var index = playerInput.playerIndex;
-        mover = movers.FirstOrDefault(m => m.GetPlayerIndex() == index);
+        var index = playerInput.playerIndex; //Get index of the Contrller
+        mover = movers.FirstOrDefault(m => m.GetPlayerIndex() == index); //organize all contrllers by index;
     }
 
     //Function that is called by controller
     public void OnMove(CallbackContext context)
     {
         if(mover !=null)
-            mover.SetInputVector(context.ReadValue<Vector2>());
+            mover.SetInputVector(context.ReadValue<Vector2>()); //Tells the mover script what value the stick motion is returning and saves it to be used later
     }
 }
