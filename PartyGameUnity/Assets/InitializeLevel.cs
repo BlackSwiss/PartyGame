2d29b88439a54c2467b3590ffdb4f530b806f230
Chloe <c.culver831@gmail.com>
2020-08-17 11:11:32 -0700

    Testing multiple inputs on Treadmill Scene

--- PartyGameUnity/Assets/InitializeLevel.cs
+++ PartyGameUnity/Assets/InitializeLevel.cs
@@ -1,33 +1,3 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class InitializeLevel : MonoBehaviour
-{
-    [SerializeField]
-    private Transform[] playerSpawns; //Setting up premade spawn locations
-
-    [SerializeField]
-    private GameObject playerPrefab; //Set player character
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        var playerConfigs = PlayerConfigurationManager.Instance.GetPlayerConfigs().ToArray();
-
-        for( int i = 0; i < playerConfigs.Length; i++)
-        {
-            //Load in player Character
-            playerConfigs[i].IsActive = false;
-            Debug.Log("Controller turned off: " + playerConfigs[i].IsActive);
-            
-            //Load in object with controller logic attached
-            var player = Instantiate(playerPrefab, playerSpawns[i].position, playerSpawns[i].rotation);
-            GameObject PlayerObj = Instantiate(playerConfigs[i].PlayerCharacter, player.transform.position, player.transform.rotation, player.transform);
-            PlayerObj.transform.parent = player.transform;
-            //Debug.Log("Player Character: " + playerConfigs[i].PlayerCharacter.name);
-            player.GetComponent<PlayerInputHandler>().InitializePlayer(playerConfigs[i]);
-        }
-    }
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:b1f21f114894c4735048219e53ed090fc2d6e27029700c83db21828dde7e32c5
+size 1337
---
+++ PartyGameUnity/Assets/Scripts/TreadmillScripts/TreadmillController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:37bf76298e237abcd4167a2dd9c6bade6bd17653b2c4cdca127b79060b2763bb
+size 7055
