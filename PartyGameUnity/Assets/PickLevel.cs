ed133f9c739f2a07fc120c91f0315740cedc0f97
Chloe <c.culver831@gmail.com>
2020-08-18 13:38:55 -0700

    Added: -Random Level Select - Countdown to next Level - Level Transition

---
+++ PartyGameUnity/Assets/PickLevel.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:754d0a7782381dcb44a96b2523ddba4af44a515ed64a29ff87fb2c923b91f5dc
+size 1455
--- PartyGameUnity/Assets/PlayerConfigurationManager.cs
+++ PartyGameUnity/Assets/PlayerConfigurationManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a5759833e4b848182f7943bf8d5c6db15bfcb170a7985410cd6bb03c793eeec6
-size 3933
+oid sha256:8d5af12fa0cd5955cbd0070d1a046a93e2274b145ab969d85db45b449546fe31
+size 3551
